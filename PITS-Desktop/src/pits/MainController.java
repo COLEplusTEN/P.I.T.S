6ac3a3c4ecf81654308d4e0ae2cdfab2a4f812b4
Sandip Subedi <sandipsubedi@outlook.com>
2016-02-17 20:37:07 -0600

    This is an auto test

--- PITS-Desktop/src/pits/MainController.java
+++ PITS-Desktop/src/pits/MainController.java
@@ -69,7 +69,7 @@ public class MainController implements Initializable{
                 new Item("Pepperoni","Package", 13.44, 11111.0, 9999.0, 19),
                 new Item("Sauce","Can",0.0, 0.0,15.35, 15),
                 new Item("Black Olives","Package",0.0, 5.33, 0.0, 44),
-                new Item("Beef","Pounds",5.39,5.24,0.0, 20)
+                new Item("Beef","Pounds",5.39,5.24,0.0, 20),
                 new Item("Beef","Pounds",5.39,5.24,0.0, 20),
                 new Item("Sandip","Weight",1.1,1.1,1.1,33)
         );
